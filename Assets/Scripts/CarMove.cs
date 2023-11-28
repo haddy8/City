b2310c94b1fe790b74832140b70df2c69c598654
haddy8 <152276707+haddy8@users.noreply.github.com>
2023-11-28 18:24:28 +0700

    Initial commit

---
+++ Assets/Scripts/CarMove.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+  public float speed;
+  public float verticalAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      verticalAxis=Input.GetAxis("Vertical");
+      if(Input.GetKey(KeyCode.W))
+    {
+       transform.Translate (Vector3.forward*Time.deltaTime*speed);
+    }
+     if(Input.GetKey(KeyCode.S))
+    {
+       transform.Translate (Vector3.back*Time.deltaTime*speed);
+    }
+     if(Input.GetKey(KeyCode.D))
+    {
+       transform.Translate (Vector3.right*Time.deltaTime*speed);
+    }
+         if(Input.GetKey(KeyCode.A))
+    {
+       transform.Translate (Vector3.left*Time.deltaTime*speed);
+      //transform.position=transform.position+(new Vector3(0, 0, -1)*Time.deltaTime*100);
+    }
+      if(Input.GetKey(KeyCode.E))
+    {
+       transform.Translate (Vector3.down*Time.deltaTime*speed);
+    }
+      if(Input.GetKey(KeyCode.Q))
+    {
+       transform.Translate (Vector3.up*Time.deltaTime*speed);
+    }
+    }
+}
